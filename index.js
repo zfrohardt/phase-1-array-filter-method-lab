2392509a74752d3ec75c3e676c5c39877958f144
Zach Frohardt <zfrohardt@gmail.com>
2021-04-06 11:42:00 -0700

    improved function definitions to use arrow syntax and implicit return

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 // Code your solution here
 let findMatching = (arr, string) => {
-    return arr.filter(function(entry) {
-        return entry.toLowerCase() === string.toLowerCase();
-    });
+    return arr.filter(entry => (
+        entry.toLowerCase() === string.toLowerCase()
+    ));
 }
 
 let fuzzyMatch = (arr, string) => {
-    return arr.filter(function(entry) {
-        return entry.slice(0, string.length) === string;
-    });
+    return arr.filter(entry => (
+        entry.slice(0, string.length) === string
+    ));
 }
 
 let matchName = (arr, string) => {
-    return arr.filter(function(entry) {
-        return entry.name === string;
-    });
+    return arr.filter(entry => (
+        entry.name === string
+    ));
 }

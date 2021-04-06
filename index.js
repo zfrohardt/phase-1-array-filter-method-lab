4493af52723e9ac4987df1498eaf9b8e1b03ebdd
Zach Frohardt <zfrohardt@gmail.com>
2021-04-06 11:01:32 -0700

    finished assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+let findMatching = (arr, string) => {
+    return arr.filter(function(entry) {
+        return entry.toLowerCase() === string.toLowerCase();
+    });
+}
+
+let fuzzyMatch = (arr, string) => {
+    return arr.filter(function(entry) {
+        return entry.slice(0, string.length) === string;
+    });
+}
+
+let matchName = (arr, string) => {
+    return arr.filter(function(entry) {
+        return entry.name === string;
+    });
+}
